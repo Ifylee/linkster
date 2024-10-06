8c93e2076c5de65fd71115d7083b660393b6dbcc
Ifylee <159982321+Ifylee@users.noreply.github.com>
2024-10-06 10:21:02 -0500

    debugging

--- models/Thought.js
+++ models/Thought.js
@@ -1,39 +1,41 @@
-const { Schema, model } = require('mongoose');   
-const reactionSchema = require('./Reaction');
-const dateFormat = require('../utils/dateFormat');  
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+const ReactionSchema = require('./Reaction');
 
-const thoughtSchema = new Schema(
-    {
-        thoughtText: {
-            type: String,
-            required: true,
-            minlength: 1,
-            maxlength: 280
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: timestamp => dateFormat(timestamp)
-        },
-        username: {
-            type: String,
-            required: true
-        },
-        reactions: [reactionSchema]  
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: 'Please enter a thought!',
+      minlength: 1,
+      maxlength: 280,
     },
-    {
-        toJSON: {
-            virtuals: true,  
-            getters: true
-        },
-        id: false
-    }
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: timestamp => dateFormat(timestamp),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [ReactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+    id: false,
+  }
 );
 
-thoughtSchema.virtual('reactionCount').get(function() {
-    return this.reactions.length;
+// Virtual to count reactions
+ThoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
 });
 
-const Thought = model('Thought', thoughtSchema);
+const Thought = model('Thought', ThoughtSchema);
 
 module.exports = Thought;
+
