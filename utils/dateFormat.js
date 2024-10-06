0252351a08c14039845a808102f2441241f2fb84
Ifylee <159982321+Ifylee@users.noreply.github.com>
2024-10-05 19:23:25 -0500

    debug

--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -1,7 +1,5 @@
 const addDateSuffix = date => {
     let dateStr = date.toString();
-
-
     const lastChar = dateStr.charAt(dateStr.length - 1);
 
     if (lastChar === '1' && dateStr !== '11') {
@@ -16,14 +14,13 @@ const addDateSuffix = date => {
     return dateStr; 
 }
 
-
 module.exports = (Timestamp, { monthLength = 'short', dateSuffix = true } = {}) => {
     const months = {
         0: monthLength === 'short' ? 'Jan' : 'January',
         1: monthLength === 'short' ? 'Feb' : 'February',
         2: monthLength === 'short' ? 'Mar' : 'March',
         3: monthLength === 'short' ? 'Apr' : 'April',
-        4: monthLength === 'short' ? 'May' : 'May',
+        4: 'May', 
         5: monthLength === 'short' ? 'Jun' : 'June',
         6: monthLength === 'short' ? 'Jul' : 'July',
         7: monthLength === 'short' ? 'Aug' : 'August',
@@ -34,24 +31,19 @@ module.exports = (Timestamp, { monthLength = 'short', dateSuffix = true } = {}) 
     };
 
     const dateObj = new Date(Timestamp);
-
     const formattedMonth = months[dateObj.getMonth()];
 
-    let dayOfMonth = dateSuffix? addDateSuffix(dateObj.getDate()) : dateObj.getDate();
+    let dayOfMonth = dateSuffix ? addDateSuffix(dateObj.getDate()) : dateObj.getDate();
+    const year = dateObj.getFullYear();
 
-    const year = dateObj.getFullYear(); 
-    // he did -12 because he wanted to convert the 24 hour time to 12 hour time
-    let hour = dateObj.getHours() > 12 ? Math.floor(dateObj.getHours() / 2) : dateObj.getHours();   
-    if (hour === 0) {
-        hour = 12;
-    }           // if hour is 0, set it to 12   
-    
+    //convert 24-hour time to 12-hour time
+    let hour = dateObj.getHours() % 12; 
+    hour = hour === 0 ? 12 : hour;  // If hour is 0, set it to 12
+
     const minutes = (dateObj.getMinutes() < 10 ? '0' : '') + dateObj.getMinutes(); // if minutes is less than 10, add a 0 in front of it
-
     const periodOfDay = dateObj.getHours() >= 12 ? 'pm' : 'am'; // if hour is greater than or equal to 12, set it to pm, else am    
 
     const formattedTimeStamp = `${formattedMonth} ${dayOfMonth}, ${year} at ${hour}:${minutes} ${periodOfDay}`;
 
     return formattedTimeStamp;  
-
 }
